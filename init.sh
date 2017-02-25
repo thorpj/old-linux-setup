3dcdf04d4624ff5aa20baf4444577e69bf4337de
Joe Thorpe <thorpejoe4@gmail.com>
2017-02-25 18:09:43 +0800

    more changes
    Former-commit-id: f6da9788a195bbf01e5ba8fd6d84b3156d271c0f

--- init.sh
+++ init.sh
@@ -8,7 +8,10 @@ ssh_key=$(cat $HOME/.ssh/id_rsa.pub)
 echo -e "\n"
 echo $ssh_key
 echo -e "\nAdd this ssh key to your Github account, if you haven't already."
-read -p "Press enter once you have added the ssh key to your Github account: "
+read -p "Press enter once you have added the ssh key to your Github account. Press enter now if you already have. "
 mkdir /home/$sys_username/Git
-git clone git@github.com:thorpj/OS-Setup.git $HOME/Git/OS-Setup
-sudo /home/$sys_username/Git/OS-Setup/linux-setup.sh
+if [ ! -f /home/$sys_username/Git/OS-Setup/ ]
+    then
+        git clone git@github.com:thorpj/OS-Setup.git $HOME/Git/OS-Setup
+fi
+sudo /home/$sys_username/Git/OS-Setup/linux_setup.sh
