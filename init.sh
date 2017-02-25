7c6642e57552f23123fbef0400a9d204aa2dcb6e
Joe Thorpe <thorpejoe4@gmail.com>
2017-02-25 17:45:07 +0800

    Removed automated send ssh key to github because it doesn't work
    Former-commit-id: 5391c3c17ba3e8e67d60977e88550ae8ea8c18cd

--- init.sh
+++ init.sh
@@ -5,10 +5,9 @@ if [ ! -f $HOME/.ssh/id_rsa.pub ]
         ssh-keygen -t rsa -N "" -f $HOME/.ssh/id_rsa
 fi  
 ssh_key=$(cat $HOME/.ssh/id_rsa.pub)
-read -p "Github Username: " username
-read -p "Github Password: " password
-read -p "Github Email: " email
-curl -u "$username:$password" --data '{"title":"$email","key":"$ssh_key"}' https://api.github.com/user/keys
+echo -e "\n"
+cat $ssh_key
+echo -e "\nAdd this ssh key to your Github account, if you haven't already."
 mkdir /home/$sys_username/Git
 git clone git@github.com:thorpj/OS-Setup.git $HOME/Git/OS-Setup
 sudo /home/$sys_username/Git/OS-Setup/linux-setup.sh
