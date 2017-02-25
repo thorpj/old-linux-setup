e6d84ee363f522b8e5bbe72e3d03c3ee4c02f1d6
Joe Thorpe <thorpejoe4@gmail.com>
2017-02-25 17:53:33 +0800

    init fixes
    Former-commit-id: afbd66db4f4e275d6ecc59a2babe8b38054210f0

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ ssh_key=$(cat $HOME/.ssh/id_rsa.pub)
 echo -e "\n"
 echo $ssh_key
 echo -e "\nAdd this ssh key to your Github account, if you haven't already."
-read -p "Press enter once you have added the ssh key to your Github account"
+read -p "Press enter once you have added the ssh key to your Github account: "
 mkdir /home/$sys_username/Git
 git clone git@github.com:thorpj/OS-Setup.git $HOME/Git/OS-Setup
 sudo /home/$sys_username/Git/OS-Setup/linux-setup.sh
