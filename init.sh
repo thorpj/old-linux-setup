1c6513b1374d976020456940ffb7f9e5a9938cd6
Joe Thorpe <thorpejoe4@gmail.com>
2017-02-25 17:48:59 +0800

    fixing init.sh: Script now waits for user to press enter after adding the SSH key to their Github account
    Former-commit-id: 2298578ee221b4dfe8ba3cd5169847cb38dcdaa0

--- init.sh
+++ init.sh
@@ -6,8 +6,9 @@ if [ ! -f $HOME/.ssh/id_rsa.pub ]
 fi  
 ssh_key=$(cat $HOME/.ssh/id_rsa.pub)
 echo -e "\n"
-cat $ssh_key
+echo $ssh_key
 echo -e "\nAdd this ssh key to your Github account, if you haven't already."
+read -p "Press enter once you have added the ssh key to your Github account"
 mkdir /home/$sys_username/Git
 git clone git@github.com:thorpj/OS-Setup.git $HOME/Git/OS-Setup
 sudo /home/$sys_username/Git/OS-Setup/linux-setup.sh
