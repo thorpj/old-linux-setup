f22fbe6b7f505f4aa2176be26158fb8351a62ae4
thorpj <thorpejoe4@gmail.com>
2017-02-27 00:14:20 +0800

    adding tmux_c window

--- os/ubuntu/ubuntu.sh
+++ os/ubuntu/ubuntu.sh
@@ -98,9 +98,12 @@ apt_install ()
                         echo 'export PATH="$HOME/.tmuxifier/bin:$PATH"' >> $HOME/.bashrc
                         echo "alias tmuxa='tmuxifier load-window tmux_a'" >> $HOME/.bashrc
                         echo "alias tmuxb='tmuxifier load-window tmux_b'" >> $HOME/.bashrc
+                        echo "alias tmuxc='tmuxifier load-window tmux_c'" >> $HOME/.bashrc
                         mkdir -p $HOME/.tmuxifier/layouts
                         echo -e "new_window \"tmux_a\"\nsplit_v 50 0\nsplit_h 50 0\nsplit_h 50 1" > $HOME/.tmuxifier/layouts/tmux_a.window.sh
                         echo -e "new_window \"tmux_a\"\nsplit_h 50 0" > $HOME/.tmuxifier/layouts/tmux_b.window.sh
+                        echo -e "new_window \"tmux_a\"\nsplit_v 50 0" > $HOME/.tmuxifier/layouts/tmux_c.window.sh
+
                 fi
         fi
 
