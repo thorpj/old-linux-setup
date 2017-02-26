7dd63985c36c3a5247aa5392683beee0549520ed
thorpj <thorpejoe4@gmail.com>
2017-02-27 00:12:24 +0800

    fixing alias missing

--- os/wsl/install_tmux.sh
+++ os/wsl/install_tmux.sh
@@ -8,8 +8,8 @@ cd $HOME
 git clone https://github.com/jimeh/tmuxifier.git $HOME/.tmuxifier
 echo 'eval "$(tmuxifier init -)"' >> ~/.profile
 echo 'export PATH="$HOME/.tmuxifier/bin:$PATH"' >> $HOME/.bashrc
-echo "tmuxa='tmuxifier load-window tmux_a'" >> $HOME/.bashrc
-echo "tmuxb='tmuxifier load-window tmux_b'" >> $HOME/.bashrc
+echo "alias tmuxa='tmuxifier load-window tmux_a'" >> $HOME/.bashrc
+echo "alias tmuxb='tmuxifier load-window tmux_b'" >> $HOME/.bashrc
 mkdir -p $HOME/.tmuxifier/layouts
 echo -e "new_window \"tmux_a\"\nsplit_v 50 0\nsplit_h 50 0\nsplit_h 50 1" > $HOME/.tmuxifier/layouts/tmux_a.window.sh
 echo -e "new_window \"tmux_a\"\nsplit_h 50 0" > $HOME/.tmuxifier/layouts/tmux_b.window.sh
