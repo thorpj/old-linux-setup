7382bceefebdf8a9ef290e537f12701daeaf00cd
thorpj <thorpejoe4@gmail.com>
2017-04-25 23:45:38 +0800

    replaced old init.sh

--- init.sh
+++ init.sh
@@ -1,44 +1,61 @@
 #!/bin/bash
-sys_username=$USER
-if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
-        ssh-keygen -t rsa -N "" -f $HOME/.ssh/id_rsa
-fi  
-echo -e "\n"
-cat "$HOME/.ssh/id_rsa.pub"
-printf "\nAdd this ssh key to your Github account, if you haven't already.\n"
-read -p "Press enter to continue: "
-if [ "$(dpkg-query -l "git")" != "" ]; then
-    echo "git is not installed. Installing..."
-    sudo apt update
-    sudo apt install -y -q git 
-fi
-git config --global user.name "thorpj"
-git config --global user.email "thorpejoe4@gmail.com"
-if [ ! -f $HOME/git/ ]
-    then
-        mkdir $HOME/git
-fi
-if [ ! -f $HOME/uni-git/ ]
-    then
-        mkdir $HOME/uni-git
-fi
-if [ ! -f $HOME/git/os-setup/ ]
-    then
-        git clone git@github.com:thorpj/os-setup.git $HOME/git/os-setup
+pwd
+askyesno ()
+#Ask if user wishes to continue with given action. Can set default to yes or no
+{
+    question=$1
+    default=$2
+    if [ "$default" = true ]; then
+            options="[Y/n]"
+            default="y"
+        else
+            options="[y/N]"
+            default="n"
+    fi
+    read -p "$question $options " -r input
+    input=${input:-${default}}
+    if [[ "$input" =~ ^[yY]$ ]]; then
+        result="true"
     else
-        echo "WARNING: the directory $HOME/git/os-setup already exists"
-        echo "WARNING: the directory $HOME/git/os-setup already exists" >> $HOME/git/os-setup/os/ubuntu/log.txt
-fi
-if [ ! -f $HOME/git/linux-scripts/ ]
-    then
-        git clone git@github.com:thorpj/linux-scripts.git $HOME/git/linux-scripts
-    else
-        echo "WARNING: the directory $HOME/git/linux-scripts already exists"
-        echo "WARNING: the directory $HOME/git/linux-scripts already exists" >> $HOME/git/os-setup/os/ubuntu/log.txt
-fi
-if [ ! -f $HOME/.temp/ ]
-    then
-        mkdir $HOME/.temp
-fi
+        result="false"
+    fi
+}
 
-sudo $HOME/git/os-setup/linux_setup.sh
+initial_setup ()
+# Check if dependencies are met
+{
+    if dpkg-query -W -f'${Status}' "git" 2>/dev/null | grep -q "ok installed"; then
+        :
+    else
+        echo "git is not installed. Installing..."
+        sudo apt-get -q update
+        sudo apt-get install -y -q git 
+    fi
+    if dpkg-query -W -f'${Status}' "curl" 2>/dev/null | grep -q "ok installed"; then
+        :
+    else
+        echo "curl is not installed. Installing..."
+        sudo apt-get -q update
+        sudo apt-get install -y -q curl
+    fi
+}
+
+main ()
+{
+    source ./sens_variables.sh
+    source ./variables.sh
+    initial_setup
+    git config --global user.name "$github_user"
+    git config --global user.name "$github_email"
+    sudo mkdir -p "$loc_git"
+    sudo mkdir -p "$loc_unigit"
+    sudo mkdir -p "$user_home/.temp"
+    wget https://github.com/$github_user/linux-setup/blob/master/send_ssh_key.sh -P $user_home
+    sleep 2s
+    bash "$user_home/send_ssh_key.sh"
+    git clone git@github.com:$github_user/os-setup.git "$loc_git"
+    rm "$user_home/send_ssh_key.sh"
+    rn "$user_home/init.sh"
+    source "$loc_repo_os/${os_name}.sh"
+}
+main
