59c39e9000fb9ab7f5555d95920ca48d00f77c62
thorpj <thorpejoe4@gmail.com>
2017-04-27 02:02:08 +0800

    adding install -f

--- os/ubuntu/app_install/nonapt/google-chrome-stable.sh
+++ os/ubuntu/app_install/nonapt/google-chrome-stable.sh
@@ -2,4 +2,5 @@
 sudo apt-get update
 sudo apt-get install -y libgconf2-4 libnss3-1d libxss1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O "$user_home/.temp/google-chrome-stable_current_amd64.deb"
+sudo apt-get -f install -y 
 sudo dpkg -i "$user_home/.temp/google-chrome-stable_current_amd64.deb"
