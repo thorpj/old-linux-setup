7aca78f1ca4285cdfbd7aa9e7cbfff9305518e7b
thorpj <thorpejoe4@gmail.com>
2017-04-27 00:00:58 +0800

    updated commands

--- test/test_edit_settings.sh
+++ test/test_edit_settings.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-nano variables.sh
-nano sens_variables.sh
+script_path="`dirname \"$0\"`"         
+script_path="`( cd $script_path && pwd )`"
+script_path=${script_path::-5}
+
+
+nano "$script_path/variables.sh"
+nano "$script_path/sens_variables.sh"
 source variables.sh
 source sens_variables.sh
 nano "$loc_add_repos_list"
-nano "$loc_authorized_keys_list"
-nano "$loc_app_list"
-nano "$loc_add_repos_list"
+nano "$loc_repo/authorized_keys.txt"
+nano "$loc_app_install_dir/app_list.txt"
+nano "$loc_repo/repo_clone_list.txt"
 nano "$loc_aliases_list"
+nano "$loc_repo/vscode_desktop_settings.txt"
+nano "$loc_repo/vscode_laptop_settings.txt"
